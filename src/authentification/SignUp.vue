d8f5250a822bed2f9c7b300ea475a23c3f58272b
BrouardVictor <victor.brouard1@gmail.com>
2022-03-14 22:57:17 -0400

    Secure sign up feature added

--- src/authentification/SignUp.vue
+++ src/authentification/SignUp.vue
@@ -5,19 +5,70 @@
 
     <form @submit.prevent="signIn">
       <div class="form-group">
-        <input type="text" class="form-control" id="email" v-model="username" placeholder="Username" required>
+        <input type="email" class="form-control" id="email" v-model="email" placeholder="Email" required>
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" id="username" v-model="username" placeholder="Username" required>
       </div>
       <div class="form-group">
         <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required>
       </div>
-      <button type="submit" class="btn btn-primary">Sign In</button>
+      <div class="form-group">
+        <input type="password" class="form-control" id="confirmPassword" v-model="confirmPassword" placeholder="Confirm your password" required>
+      </div>
+      <button v-on:click="signUp();" type="submit" class="btn btn-primary">Sign In</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
+
   name: "SignUp",
+  methods: {
+    checkUsername(){
+      return /^[a-zA-Z0-9]{3,20}$/.test(document.getElementById("username").value);
+    },
+    checkPassword(){
+      return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[.:/(){}@$!%*?&])[A-Za-z\d.:/(){}@$!%*?&]{8,50}$/.test(document.getElementById("password").value);
+    },
+    checkEmail(){
+      return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById("email").value)
+    },
+    confirmPassword(){
+      return document.getElementById("confirmPassword").value().equals(document.getElementById("confirmPassword").value());
+    },
+    checkInputs(){
+      if(!this.checkEmail()){
+        alert("Invalid Email");
+        return false;
+      }else if(!this.checkUsername()){
+        alert("Username length should be from 3 to 20 and contains letters and numbers only");
+        return false
+      }else if(!this.checkPassword()){
+        alert("Invalid Password");
+        return false
+      }else if(!this.confirmPassword()){
+        alert("Password confirmation different from password");
+        return false
+      }
+      return true;
+    },
+    signUp(){
+      if(this.checkInputs()){
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({username:document.getElementById("username").value, password:document.getElementById("password").value, mail: document.getElementById("email").value})
+        };
+        fetch("http://localhost:8080/signup", requestOptions).then(response => response.json()).then((data) => {
+          if(data.token){
+            sessionStorage.setItem('token', data.token);
+          }
+        });
+      }
+    }
+  }
 }
 </script>
 
@@ -36,9 +87,9 @@ template{
   right: 0;
   margin-left: auto;
   margin-right: auto;
-  margin-top: 25vh;
+  margin-top: 18vh;
   width: 500px;
-  height: 50vh;
+  height: 67vh;
   background-color: #fff;
   border-radius: 40px;
   text-align: center;
